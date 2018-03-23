c4e3c30afeb007f10b3c374ab15d328863af7e53
cam315 <noasark@163.com>
2018-03-23 14:11:32 +0100

    calculate MgCl2 addition in PCR system
    A small function which can automatically calculte the amount of MgCl2 to the PCR reaction system, according to the total volumn, the final concentration, the concentration of MgCl2 in your buffer and in the MgCl2 stock solution.

---
+++ calc MgCl2.R
@@ -0,0 +1,42 @@
+## A simple function to calculate MgCl2 concentrations in PCR
+## Sizhong, 2014-3-10
+
+magnisium <- function(volumn, finalconc,times_buffer =10, Mg_in_buffer = 15,wsolution = 25) {
+    finalconc = seq(1.5,5,by = 0.25) # unit mM
+    x = list()
+    for (i in volumn) {
+        x[[i]] = (finalconc*i- i/times_buffer*Mg_in_buffer)/wsolution
+        names(x[[i]]) = finalconc
+    }
+    return(x)    
+}
+
+
+# remove empty element 
+
+delete.nulls  <-  function(x.list){   # delele null/empty entries in a list
+    x.list[unlist(lapply(x.list, length) != 0)]
+}
+
+
+# Usage
+# volumn: parameter specifying the volumn of the PCR reaction system, e.g. 25µl,50µl;
+# finalconc: the final concentration (mM) of MgCl2 you want to reach; 
+# times_buffer: the Reaction Buffer solution, for example 10 × Pol Buffer B for OptiTag from roboklon (see http://www.roboklon.de/pdf/7_en.pdf for this Buffer);
+# Mg_in_buffer: the Mg concentration already in buffer, e.g. 10 x Pol Buffer B (general 
+application, up to 8-10 kb) contains 15 mM MgCl2
+# wsolution: the Mg concentration of your stock solution, e.g. 25mM.
+
+# use function
+
+a = magnisium(c(25,50))
+
+b = delete.nulls(a)
+
+# merge to a final data frame
+
+cc = do.call(rbind, b)
+rownames(cc) <- paste0(totalvolumn,'µl')
+
+write.table(cc, file='MgCl2_to_be_added.txt',quote=F,sep="\t")
+
