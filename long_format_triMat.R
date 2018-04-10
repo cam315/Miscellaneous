23598943fbaf577661f6c30b5c098ef2c5be8707
camel315 <syang@gfz-potsdam.de>
2018-04-10 10:50:43 +0200

    change parameter

--- long_format_triMat.R
+++ long_format_triMat.R
@@ -1,11 +1,11 @@
 ## convert lower triangular matrix to long format data frame with 3 columns
 ## Sizhong Yang, 2013-3-18, Potsdam
 
-convert <- function(gene, colname=c("sp1","sp2","dist"), digs =4,...){
+convert <- function(input, colname=c("sp1","sp2","dist"), digs =4,...){
     # digs specifies the number of decimal places
-    if(!is.matrix(gene) | !is.data.frame(gene)) gene = as.matrix(gene)
+    if(!is.matrix(input) | !is.data.frame(input)) input = as.matrix(input)
     x <- data.frame(matrix(data=NA,ncol=3,byrow=T))
-    name <- rownames(gene)
+    name <- rownames(input)
     #name <- gsub(" ", "",name)
     n <- length(name)
     q = 1
@@ -13,7 +13,7 @@ convert <- function(gene, colname=c("sp1","sp2","dist"), digs =4,...){
         for (j in (1:i-1)){
             spa <- name[i]  
             spb <- name[j] 
-            value <- round(gene[i,j], digits = digs)
+            value <- round(input[i,j], digits = digs)
             x[q,1:3] <- c(spa,spb,value)
             q = q+1
          }
