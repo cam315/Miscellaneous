fecb7fed4b59f93593d0b6682bad3096b3a3c762
Sizhong Yang <37700267+camel315@users.noreply.github.com>
2018-05-04 13:37:29 +0200

    Update calc MgCl2.R

--- calc MgCl2.R
+++ calc MgCl2.R
@@ -1,30 +1,29 @@
 ## A simple function to calculate MgCl2 concentrations in PCR
 ## Sizhong, 2014-3-10
 
-magnisium <- function(volumn, finalconc,times_buffer =10, Mg_in_buffer = 15,wsolution = 25) {
-    finalconc = seq(1.5,5,by = 0.25) # unit mM
-    x = list()
-    for (i in volumn) {
-        x[[i]] = (finalconc*i- i/times_buffer*Mg_in_buffer)/wsolution
-        names(x[[i]]) = finalconc
+magnisium <- function(volumn, finalconc,times_buffer =10, Mg_in_buffer = 15, wsolution = 25) {
+    finalconc = seq(1.5,5, by = 0.25) # unit mM
+    res = vector(mode = "list", length = length(volumn))
+for (i in volumn) {
+        res[[i]] = (finalconc*i- i/times_buffer*Mg_in_buffer)/wsolution
+        names(res[[i]]) = finalconc
     }
-    return(x)    
+return(res)    
 }
 
 
 # remove empty element 
 
-delete.nulls  <-  function(x.list){   # delele null/empty entries in a list
+delete.nulls <- function(x.list){ 
     x.list[unlist(lapply(x.list, length) != 0)]
 }
 
 
 # Usage
-# volumn: parameter specifying the volumn of the PCR reaction system, e.g. 25µl,50µl;
+# volumn: parameter specifying the volumn of the PCR reaction system, e.g. 25Âµl,50Âµl;
 # finalconc: the final concentration (mM) of MgCl2 you want to reach; 
-# times_buffer: the Reaction Buffer solution, for example 10 × Pol Buffer B for OptiTag from roboklon (see http://www.roboklon.de/pdf/7_en.pdf for this Buffer);
-# Mg_in_buffer: the Mg concentration already in buffer, e.g. 10 x Pol Buffer B (general 
-application, up to 8-10 kb) contains 15 mM MgCl2
+# times_buffer: the Reaction Buffer solution, for example 10 Ã— Pol Buffer B for OptiTag <br/>from roboklon (see http://www.roboklon.de/pdf/7_en.pdf for this Buffer);
+# Mg_in_buffer: the Mg concentration already in buffer, e.g. 10 x Pol Buffer B <br/>(general application, up to 8-10 kb) contains 15 mM MgCl2
 # wsolution: the Mg concentration of your stock solution, e.g. 25mM.
 
 # use function
@@ -36,7 +35,7 @@ b = delete.nulls(a)
 # merge to a final data frame
 
 cc = do.call(rbind, b)
-rownames(cc) <- paste0(totalvolumn,'µl')
+rownames(cc) <- paste0(totalvolumn,'Âµl')
 
-write.table(cc, file='MgCl2_to_be_added.txt',quote=F,sep="\t")
+write.table(cc, file='MgCl2_to_be_added.txt',quote=FALSE, sep="\t")
 
